3992502e80e825e9888105a74fd2ca808752bdca
Pravin Patil <106026547+pravin8007@users.noreply.github.com>
2023-11-14 12:02:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 function stringChop(str, size) {
   // your code here
-	let i = 0 , ans = [];
+	if(str === null){
+		return [];
+	}
+	let i = 0 ;
+	let ans = [];
 	while(i <= str.length-1){
-		ans.push(str.slice(i,i+2));
-		i= i+2;
+		ans.push(str.slice(i , i+ size));
+		i = i+ size ;
 	}
 	return ans;
 	
@@ -12,4 +16,6 @@ function stringChop(str, size) {
 // // Do not change the code below
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
+
 alert(stringChop(str, size));
+
